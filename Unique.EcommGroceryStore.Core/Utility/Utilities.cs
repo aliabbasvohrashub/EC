f78059f204d23adf23fded4f5f810d3cce625647
Zishanali Tailor <zt@baroda.sfwltd.co.uk>
2016-05-18 12:52:33 +0530

    Added Membership provider and Role Provider.

---
+++ Unique.EcommGroceryStore.Core/Utility/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unique.EcommGroceryStore.Core.Utility
+{
+    public class Utilities
+    {
+        public static string GetIPAddress
+        {
+            get
+            {
+                string hostName = Dns.GetHostName(); // Retrive the Name of HOST
+                return Dns.GetHostByName(hostName).AddressList[0].ToString();
+            }
+        }
+    }
+}
