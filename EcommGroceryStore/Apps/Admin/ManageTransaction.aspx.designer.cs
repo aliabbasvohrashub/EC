8b02081305cdce30c9f905c6d198c8507344b45b
aliabbasvohra <aliabbasvohra@gmail.com>
2016-06-21 14:50:45 +0530

    Update manage Transaction page.

--- EcommGroceryStore/Apps/Admin/ManageTransaction.aspx.designer.cs
+++ EcommGroceryStore/Apps/Admin/ManageTransaction.aspx.designer.cs
@@ -13,6 +13,24 @@ namespace EcommGroceryStore.Apps.Admin {
     public partial class ManageTransaction {
         
         /// <summary>
+        /// lblMsg control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMsg;
+        
+        /// <summary>
+        /// ddlStatus control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlStatus;
+        
+        /// <summary>
         /// gvTransactionDetails control.
         /// </summary>
         /// <remarks>
